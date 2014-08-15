56a0c4a26fab140df018e72c9bf8a4d8f68a737b
Yancy Ribbens <yancy.ribbens@gmail.com>
2014-08-15 11:58:37 -0400

    use config for token and return drop down array

--- lib.php
+++ lib.php
@@ -1,14 +1,30 @@
 <?php
+require_once(dirname(__FILE__).'/../../config.php');
 
 function query_acclaim_api(){
+    global $DB;
     $org_id="6bb2e1c7-c66b-4d47-9301-4a6b9e792e2c";
     $url="https://jefferson-staging.herokuapp.com/api/v1/organizations/".$org_id."/badge_templates";
+    $token = $DB->get_record('config', array('name'=>'token'));
 
     //pass token as argument so it's not in code base
-    $username = "FZ1QZ4sDtEwNR7Tcv-Yi";
+    $username = $token->value;
     $password = "";
 
     return return_json_badges($url,$username,$password);
+}
+
+
+function truncate($input){
+    $max_length = 18;
+
+    if(strlen($input) > $max_length){
+        $len = strlen($input);
+        $trim_amount = ($max_length - $len);
+        return substr($input,0, $trim_amount)."...";
+        }
+
+    return $input;
 }
 
 function return_json_badges($url,$username,$password){
@@ -28,23 +44,27 @@ function return_json_badges($url,$username,$password){
     $json = json_decode($result,true);
     $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
     curl_close($ch);
+
+    error_log("json ".print_r($json,true));
     return $json;
 }
 
 function build_radio_buttons($json){
-    if(!empty($json)){
-        $stack = array();
-        foreach($json['data'] as $item){
-            array_push($stack,
-                html_writer::tag('img', '', array('alt' => 'test', 'height' => '150', 'width' => '150' ,'src' => $item["image_url"]))
-                );
-            }
+     $badge_items = "";
+     
+     if(isset($json['data'])){
+         $badge_items = array();
+         foreach($json['data'] as $item){
+             $friendly_name = truncate($item["name"]);
+             $badge_id = $item["id"];
+             $badge_items[$badge_id] = $friendly_name;
+             }
         }else{
-            //put in the invalid url
             error_log("invalid api, token or unable to connect");
-        }
-    return $stack;
+            }
+    return $badge_items;
 }
+
 
 function block_acclaim_images() {
     $json = query_acclaim_api();
