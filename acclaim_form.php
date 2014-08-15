a9910f8fc7f157cf2a0f1329994557379f8264db
Yancy Ribbens <yancy.ribbens@gmail.com>
2014-08-15 12:02:18 -0400

    create drop down list

--- acclaim_form.php
+++ acclaim_form.php
@@ -12,14 +12,21 @@ class acclaim_form extends moodleform {
 	$mform->addElement('header','displayinfo', 'Acclaim Badges');
  
 	// add image selector radio buttons
-	$images = block_acclaim_images();
-	$radioarray = array();
-	for ($i = 0; $i < count($images); $i++) {
-    	    $radioarray[] =& $mform->createElement('radio', 'picture', '', $images[$i], $i);
-	}
+	$badge_items = block_acclaim_images();
+	//$radioarray = array();
 
-	$mform->addGroup($radioarray, 'radioar', 'Badge Select', array(' '), FALSE);
-	
+	//for ($i = 0; $i < count($images); $i++) {
+    	//    $radioarray[] =& $mform->createElement('radio', 'picture', '', $images[$i], $i);
+	//}
+
+	//$mform->addGroup($radioarray, 'radioar', 'Badge Select', array(' '), FALSE);
+        
+        //$FORUM_TYPES = array();
+        //$FORUM_TYPES["foo"] = "bar";
+        //$FORUM_TYPES["bar"] = "foo";
+
+        $mform->addElement('select', 'type', 'forum', $badge_items, '');
+        	
 	// add optional grouping
 	$mform->addElement('header', 'optional', 'optional', null, false); // add date_time selector in optional area
 	$mform->addElement('date_time_selector', 'displaydate', 'Expires', array('optional' => true));
@@ -28,7 +35,7 @@ class acclaim_form extends moodleform {
         // hidden elements
         $mform->addElement('hidden', 'blockid');
         $mform->addElement('hidden', 'courseid');
-        	
+	
 	$this->add_action_buttons();
 	}
 }
