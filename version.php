f554c762a6400591d5ea94285e9d7f036ad34cc0
Yancy Ribbens <yancy.ribbens@gmail.com>
2014-08-25 22:58:26 -0400

    update version number

--- version.php
+++ version.php
@@ -1,3 +1,3 @@
 <?php
 $plugin->version = 2014082500;  // YYYYMMDDHH (year, month, day, 24-hr time)
-$plugin->requires = 2014112400; // YYYYMMDDHH (This is the release version for Moodle 2.0)
+$module->requires = 2010031900;  // Requires this Moodle version
