515a79ea705e285911616a1d1664c1cf2cfc32d5
Yancy Ribbens <yancy.ribbens@gmail.com>
2014-08-27 22:42:42 -0400

    remove debug message

--- view.php
+++ view.php
@@ -30,19 +30,13 @@ if(!empty($courseid)){
 $acclaim->set_data($toform);
 
 if($acclaim->is_cancelled()) {
-    // Cancelled forms redirect to the course main page.
-    //error_log('is canceled');
     $courseurl = new moodle_url('/course/view.php', array('id' => $id));
     redirect($courseurl);
 } else if ($fromform = $acclaim->get_data()) {
-    //error_log('save data');
-    // We need to add code to appropriately act on and store the submitted data
-    // but for now we will just redirect back to the course main page.
     $courseurl = new moodle_url('/course/view.php', array('id' => $courseid));
-    print_object($fromform);
-    //redirect($courseurl);
+    //print_object($fromform);
+    redirect($courseurl);
 } else {
-    //error_log('not conceled or not data');
     // form didn't validate or this is the first display
     $site = get_site();
     echo $OUTPUT->header();
