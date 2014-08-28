ad823dace554d0e427d0b03c7408e162e363e426
Yancy Ribbens <yancy.ribbens@gmail.com>
2014-08-27 22:43:05 -0400

    cleanup

--- acclaim_form.php
+++ acclaim_form.php
@@ -11,9 +11,9 @@ class acclaim_form extends moodleform {
 	//populate form 
 	$mform->addElement('header','displayinfo', 'Acclaim Badges');
 	$badge_items = block_acclaim_images();
-        $mform->addElement('select', 'type', 'forum', $badge_items, '');
+        $mform->addElement('select', 'badgeid', 'forum', $badge_items, '');
 	$mform->addElement('header', 'optional', 'optional', null, false); // add date_time selector in optional area
-	$mform->addElement('date_time_selector', 'displaydate', 'Expires', array('optional' => true));
+	$mform->addElement('date_time_selector', 'expiration', 'Expires', array('optional' => true));
 	$mform->setAdvanced('optional');
 
         // hidden elements
