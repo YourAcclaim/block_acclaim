932d2e88267abc03e0bb1b5ea51a93628a35e38c
Yancy Ribbens <yancy.ribbens@gmail.com>
2014-09-01 16:03:06 -0400

    issue badge working

--- classes/group_observers.php
+++ classes/group_observers.php
@@ -25,65 +25,21 @@ namespace block_acclaim;
 
 defined('MOODLE_INTERNAL') || die();
 
-//require_once($CFG->dirroot . '/mod/acclaim/locallib.php');
+require_once($CFG->dirroot . '/blocks/acclaim/lib.php');
+
 class group_observers {
 
     public static function issue_badge($event) {
 	global $DB;
-        $user_id = $event->userid;
-        $course_id = $event->courseid;
-	$user = $DB->get_record('user', array('id'=>$user_id));
-        $firstname = $user->firstname;
-        $lastname = $user->lastname;
-	$email = $user->email;
-        
-//        $api = $DB->get_record('config', array('name'=>'api'));
-//        $token = $DB->get_record('config', array('name'=>'token'));
-//        $badge_id = $DB->get_record('config', array('name'=>'badge_template_id'));
-//        $expires_at = $DB->get_record('config', array('name'=>'expires_at'));
-//        $url = $api->value;
-
-	//$block_record = $DB->get_record('block_acclaim', array('courseid' => '4');
-	$course = $DB->get_record('block_acclaim', array('courseid' => '10'), '*', MUST_EXIST);
-        //error_log('User Record: '.print_r($user,true));
-        //error_log('url'.print_r($url,true));
-  //      error_log('Event Data: ' . print_r($event,true));
-	error_log('Block Record: ' . print_r($course,true));
-
-//        $date_time = date('Y-m-d  h:i:s a', time());
-//        $password = "";
-
-//        $data = array(
-//            'badge_template_id' => $badge_id->value,
-//            'issued_to_first_name' => $user->firstname,
-//            'issued_to_last_name' => $user->lastname,
-//            'expires_at' => $expires_at->value,
-//            'recipient_email' => $user->email,
-//            'issued_at' => $date_time
-//        );
-
- //       error_log('Data: '.print_r($data,true));
-        //error_log('api_url: '.$api->value);
-
- //       $ch = curl_init();
-
-//        $curlConfig = array(
-//            CURLOPT_HTTPHEADER     => array('Accept: application/json'),
-//            CURLOPT_CUSTOMREQUEST  => "POST",
-//            CURLOPT_FOLLOWLOCATION => true,
-//            CURLOPT_URL            => $api->value,
-//            CURLOPT_RETURNTRANSFER => true,
-//            CURLOPT_USERPWD        => $token->value . ":" . $password,
-//            CURLOPT_POSTFIELDS     => $data,
-//        );
-
-//        error_log('curl_array: '. print_r($curlConfig,true));
-//        curl_setopt_array($ch, $curlConfig);
-
-//        $result = curl_exec($ch);
- //       $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-  //      curl_close($ch);
+        $badge_id = get_badge_id($event);
+        $data = create_data_array($event,$badge_id,"");
+        $url = get_issue_badge_url();
+        $token = get_request_token();
+        $return_code = issue_badge_request($data,$url,$token);
     }
 }
 
 ?>
+
+
+
