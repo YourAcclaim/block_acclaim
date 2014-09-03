6b1f8f5e10d7790d7d9eb1d83adbf9b7a0a48dd6
Yancy Ribbens <yancy.ribbens@gmail.com>
2014-09-03 19:31:58 -0400

    fix cancel button

--- view.php
+++ view.php
@@ -7,6 +7,9 @@ require_once($CFG->dirroot . '/blocks/acclaim/lib.php');
 global $DB, $OUTPUT, $PAGE;
 
 $courseid = required_param('courseid', PARAM_INT);
+$blockid = required_param('blockid', PARAM_INT);
+
+error_log("courseid: ".$courseid);
 
 if (!$course = $DB->get_record('course', array('id' => $courseid))) {
     print_error('invalidcourse', 'block_acclaim', $courseid);
@@ -19,18 +22,18 @@ $PAGE->set_pagelayout('standard');
 
 $acclaim = new acclaim_form();
 
-if(!empty($blockid)){
-    $toform['blockid'] = $blockid;
-}
+//if(!empty($blockid)){
+$toform['blockid'] = $blockid;
+//}
 
-if(!empty($courseid)){
-    $toform['courseid'] = $courseid;
-}
+//if(!empty($courseid)){
+$toform['courseid'] = $courseid;
+//}
 
 $acclaim->set_data($toform);
 
 if($acclaim->is_cancelled()) {
-    $courseurl = new moodle_url('/course/view.php', array('id' => $id));
+    $courseurl = new moodle_url('/course/view.php', array('id' => $courseid));
     redirect($courseurl);
 } else if ($fromform = $acclaim->get_data()) {
     $courseurl = new moodle_url('/course/view.php', array('id' => $courseid));
