b392ac57d035cb6f11da10e3bfa58388ed841cf5
root <yancy.ribbens@gmail.com>
2016-08-24 17:47:16 -0400

    add some test content

--- block_acclaim.php
+++ block_acclaim.php
@@ -1,5 +1,19 @@
 <?php
 class block_acclaim extends block_base{
     public function init(){
+        $this->title = get_string('acclaim', 'block_acclaim');
+    }
+
+    public function get_content(){
+        if ($this->content !== null) {
+            return $this->content;
+        }
+
+        $this->content         =  new stdClass;
+        $this->content->text   = 'The content of our Acclaim block!';
+        $this->content->footer = 'Footer here...';
+                 
+        return $this->content;
+    }
             
 }
