082a626351ee5e72a73d1019ada3d6949859f963
root <yancy.ribbens@gmail.com>
2016-08-24 17:31:59 -0400

    add a new block

---
+++ lang/en/block_acclaim.php
@@ -0,0 +1,5 @@
+<?php
+$string['pluginname'] = 'Acclaim block';
+$string['acclaim'] = 'Acclaim';
+$string['simplehtml:addinstance'] = 'Add a new Acclaim block';
+$string['simplehtml:myaddinstance'] = 'Add a new Acclaim block to the My Moodle page';
